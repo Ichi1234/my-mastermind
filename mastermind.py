a4586aaee3d9ae95add8e026ffdf3dce9eca0177
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2023-11-16 00:27:25 +0700

    found bug in clue method

--- mastermind.py
+++ mastermind.py
@@ -3,55 +3,73 @@ import random
 class Boards:
 
     def __init__(self):
-        self.colors = ["1", "2", "3", "4", "5", "6", "7", "8"]
-        self.positions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        self.answer = [random.choice(self.colors),random.choice(self.colors),
-                       random.choice(self.colors), random.choice(self.colors)]
-        print(self.answer) ###TODO delete this when finish
-        self.round = 1
-    def clues(self, user_num):
-        list_user = list(user_num)
+        #user setup
+        self.colors_num = int(input("How many colors? (1-8): "))
+        while self.colors_num > 8:
+            self.colors_num = int(input("How many colors? (1-8): "))
 
+        self.positions_num = int(input("How many positions? (1-10): "))
+        while self.positions_num > 10:
+            self.positions_num = int(input("How many positions? (1-10): "))
+        #color
+        self.colors = []
+        self.add_colors()
+
+        #The solution is here
+        self.answer = ["3", "3", "1", "3"]
+        # self.random_answer()
+
+        print(self.answer) ###TODO delete this when finish
+        #How many round user play
+        self.round = 1
+
+    def add_colors(self):
+        for i in range(self.positions_num):
+            if i < self.colors_num:
+               self.colors.append(str(i+1))
+            else:
+               self.colors.append(random.randint(1, self.colors_num))
+
+    def random_answer(self):
+        for i in range(self.positions_num):
+            self.answer.append(random.choice(self.colors))
+
+    def clues(self, user_num):
+
+        list_user = list(user_num)
         user_clues = ""
         if list_user == self.answer:
             return None
         else:
+            check = []
             for i in range(len(self.answer)):
                  if list_user[i] == self.answer[i]:
                          user_clues += "0"
-                 elif list_user[i] in self.answer:
+                         check.append(list_user[i])
+                 elif list_user[i] in self.answer and list_user[i] not in check:
                          user_clues += "X"
+                         check.append(list_user[i])
                  else:
                      user_clues += "."
-        random_list = []
-        list_user_clue = list(user_clues)
-        for j in range(len(user_clues)):
-            random_choice = random.choice(list_user_clue)
-            list_user_clue.remove(random_choice)
-            random_list.append(random_choice)
+
+        #swap string place
+        random_list = random.sample(user_clues, len(user_clues))
         self.round += 1
 
         return "".join(random_list)
 
-
-###TODO Make user setup
-# colors_num = input("How many colors? ")
-# positions_num = input("How many positions? ")
-
-# print(f"Playing Mastermind with {colors_num} colors and {positions_num} positions")
-
+#main
 board = Boards()
+
 while True:
-#    user_guess = input("What is your guess ")
-#    print(f"Your guess is {user_guess}")
-
-
-    user_guess = input("What is your guess ")
+    user_guess = input("What is your guess?: ")
     print(f"Your guess is {user_guess}")
     clue = board.clues(user_guess)
     if clue is None:
         print(f"You solve it after {board.round} rounds")
+        print()
         board = Boards()
+
     else:
         print(clue)
     print()
