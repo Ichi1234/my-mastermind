0c62341c6c8d7235671ae0a589ddc7995d084a79
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2023-11-15 23:08:18 +0700

    Finish clue method

--- mastermind.py
+++ mastermind.py
@@ -3,17 +3,18 @@ import random
 class Boards:
 
     def __init__(self):
-        self.colors = [1, 2, 3, 4, 5, 6, 7, 8]
+        self.colors = ["1", "2", "3", "4", "5", "6", "7", "8"]
         self.positions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        # self.answer = [random.choice(self.colors),random.choice(self.colors),
-        #                random.choice(self.colors), random.choice(self.colors)]
-        self.answer = ["1", "2", "3", "4"]
+        self.answer = [random.choice(self.colors),random.choice(self.colors),
+                       random.choice(self.colors), random.choice(self.colors)]
+        print(self.answer) ###TODO delete this when finish
+        self.round = 1
     def clues(self, user_num):
         list_user = list(user_num)
 
         user_clues = ""
         if list_user == self.answer:
-            return "WIN"
+            return None
         else:
             for i in range(len(self.answer)):
                  if list_user[i] == self.answer[i]:
@@ -22,7 +23,15 @@ class Boards:
                          user_clues += "X"
                  else:
                      user_clues += "."
-        return user_clues
+        random_list = []
+        list_user_clue = list(user_clues)
+        for j in range(len(user_clues)):
+            random_choice = random.choice(list_user_clue)
+            list_user_clue.remove(random_choice)
+            random_list.append(random_choice)
+        self.round += 1
+
+        return "".join(random_list)
 
 
 ###TODO Make user setup
@@ -31,10 +40,18 @@ class Boards:
 
 # print(f"Playing Mastermind with {colors_num} colors and {positions_num} positions")
 
-# while True:
+board = Boards()
+while True:
 #    user_guess = input("What is your guess ")
 #    print(f"Your guess is {user_guess}")
-board = Boards()
-user_guess = input("What is your guess ")
-print(f"Your guess is {user_guess}")
-print(board.clues(user_guess))
+
+
+    user_guess = input("What is your guess ")
+    print(f"Your guess is {user_guess}")
+    clue = board.clues(user_guess)
+    if clue is None:
+        print(f"You solve it after {board.round} rounds")
+        board = Boards()
+    else:
+        print(clue)
+    print()
